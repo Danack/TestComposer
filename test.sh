1209e9072401988319f8154678eb608c703470e1
Danack <Danack@basereality.com>
2015-12-15 17:05:27 +0000

    Separate files out.

--- test.sh
+++ test.sh
@@ -12,29 +12,29 @@ current_pid=$$
 
 echo "current_pid is ${current_pid}"
 
-# Start a PHP server
-valgrind --tool=callgrind \
-  --instr-atstart=no \
-  --callgrind-out-file=composer.callgrind \
-  php -S 0.0.0.0:80 -t ./public > valgrind_output_.txt 2>&1 &
 
-# Grab the process ID of the just started server.
-server_pid=$!
-echo "Server PID is ${server_pid}"
-
-  #PHP needs a moment to start accepting requests
-  sleep 1
 
 for test_setting in "${test_settings[@]}"
 do
 
   echo "Testing with setting ${test_setting}"
+  
+  # Start a PHP server
+  valgrind --tool=callgrind \
+    --instr-atstart=no \
+    --callgrind-out-file="cachegrind.out.${test_setting}" \
+    php -S 0.0.0.0:80 -t ./public > valgrind_output_.txt 2>&1 &
+
+  # Grab the process ID of the just started server.
+  server_pid=$!
+  echo "Server PID is ${server_pid}"
+
+  #PHP needs a moment to start accepting requests
+  sleep 1
 
   # Comment in to debug the server
   # read -p "php is running - press [Enter] to stop"
   
-
-
   setup_check=$(wget -q "http://127.0.0.1/?test=check&setting=${test_setting}" -O -)
   
   if [ "${setup_check}" != "Ok ${test_setting}" ];
@@ -62,6 +62,9 @@ do
     # callgrind_control status
     callgrind_control -i off
   fi
+  
+  pkill php
+  pkill callgrind-amd64
 
 done
 
@@ -69,9 +72,8 @@ done
 # Kill the PHP server
 # Something is wrong with the PIDs
 #kill "${server_pid}"
-pkill php
-pkill callgrind-amd64
+
 
 
 # Debugging for when the server fails to stop.
-echo "Pid was ${server_pid}"
+# echo "Pid was ${server_pid}"
